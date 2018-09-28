422f83f0cf43e4bfdc822d1a19053d2b508c0bbf
Nabetse28 <nabetse.hv.09@hotmail.com>
2018-09-27 20:14:17 -0600

    Faltan los 3 modulos unidos

--- PipelineProcessor/PCreg.sv
+++ PipelineProcessor/PCreg.sv
@@ -1,9 +1,13 @@
-module PCreg #	(	parameter N = 8) (	input logic clk, reset,
+module PCreg #	(	parameter N = 8) (	input logic clk, reset, en,
 													input logic [N-1:0] inPC,
 													output logic [N-1:0] outPC);
 	
-always_ff @(posedge clk, posedge reset)
+always_ff @(posedge clk, posedge reset) begin
+	
 	if (reset) outPC <= 0;
-	else outPC <= inPC;
+	
+	else if (~en) outPC <= inPC;
+	
+	end
 	
 endmodule

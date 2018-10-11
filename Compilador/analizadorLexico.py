03446c28ed6d7444fce79c465ff714bef411d858
Nabetse28 <nabetse.hv.09@hotmail.com>
2018-10-11 15:27:05 -0600

    Este es el compilador

---
+++ Compilador/analizadorLexico.py
@@ -0,0 +1,148 @@
+import ply.lex as lex
+import re
+import codecs
+import os
+import sys
+
+clase = 0
+
+errL = False
+isComment = 0
+
+# Tokens para catalogar los lexemas
+tokens = ['ID','RPC','LPC','NUMBER','REG','COMMA','MINUS','NUM','DP']
+
+
+kwords = ['ADD','SUB','MUL','B','BEQ','BNE','BGT',
+          'BLT','CMP','STR','LDR',
+          'MOV','AND','ORR','PRM','UMB','LSL']
+
+tokens = tokens + kwords
+
+t_RPC = r'\]'
+t_LPC = r'\['
+t_COMMA = r','
+t_MINUS = r'\-'
+t_NUM = r'\#'
+t_DP = r'\:'
+
+def t_ADD(t):
+    r'ADD|add'
+    t.value = t.value.upper()
+    return t
+
+def t_SUB(t):
+    r'SUB|sub'
+    t.value = t.value.upper()
+    return t
+
+def t_MUL(t):
+    r'MUL|mul'
+    t.value = t.value.upper()
+    return t
+def t_B(t):
+    r'B|b'
+    t.value = t.value.upper()
+    return t
+def t_BEQ(t):
+    r'BEQ|beq'
+    t.value = t.value.upper()
+    return t
+def t_BNE(t):
+    r'BNE|bne'
+    t.value = t.value.upper()
+    return t
+def t_BGT(t):
+    r'BGT|bgt'
+    t.value = t.value.upper()
+    return t
+def t_BLT(t):
+    r'BLT|blt'
+    t.value = t.value.upper()
+    return t
+
+def t_CMP(t):
+    r'CMP|cmp'
+    t.value = t.value.upper()
+    return t
+
+def t_STR(t):
+    r'STR|str'
+    t.value = t.value.upper()
+    return t
+
+def t_LDR(t):
+    r'LDR|ldr'
+    t.value = t.value.upper()
+    return t
+
+
+def t_MOV(t):
+    r'MOV|mov'
+    t.value = t.value.upper()
+    return t
+
+def t_ORR(t):
+    r'ORR|orr'
+    t.value = t.value.upper()
+    return t
+
+def t_AND(t):
+    r'AND|and'
+    t.value = t.value.upper()
+    return t
+
+def t_PRM(t):
+    r'PRM|prm'
+    t.value = t.value.upper()
+    return t
+
+def t_UMB(t):
+    r'UMB|umb'
+    t.value = t.value.upper()
+    return t
+
+def t_LSL(t):
+    r'LSL|lsl'
+    t.value = t.value.upper()
+    return t
+
+def t_ID(t):
+    r'[a-qs-zA-QS-Z][a-zA-Z0-9]*'
+    return t
+
+def t_REG(t):
+    r'[rR][0-9]+'
+    t.value = t.value.upper()
+    return t
+
+def t_COMMENT(t):
+    r';.*'
+    global isComment
+    isComment += 1
+    t.lexer.skip(1)
+
+def t_NUMBER(t):
+    r'\d+|\-\d+'
+    return t
+
+def t_error(t):
+    global errL
+    errL = True
+    print("Aqui hay un error",t.value[0])
+    t.lexer.skip(1)
+    
+def t_nonespace(t):
+    r'\s+'
+    pass
+
+def getErrL():
+    global errL
+    return errL
+
+
+
+
+
+
+        
---
+++ Compilador/cv.s
@@ -0,0 +1,7 @@
+      MOV R1,#7
+      MOV R3,#10
+
+      ADD R1,R1,R3
+LOOP: MOV R4,#4
+      SUB R4,R4,R1
+ 
